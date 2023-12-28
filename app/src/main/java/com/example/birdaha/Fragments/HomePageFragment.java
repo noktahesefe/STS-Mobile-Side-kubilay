70edc20702dfecdb9e80bdb588897b3649b6297a
Kubilay Yazman <kubilayyazman@gmail.com>
2023-12-28 04:14:07 +0300

    Added homepage
    I designed the homepage alike our Figma design.

--- app/src/main/java/com/example/birdaha/Fragments/HomePageFragment.java
+++ app/src/main/java/com/example/birdaha/Fragments/HomePageFragment.java
@@ -1,16 +1,23 @@
 package com.example.birdaha.Fragments;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.AnimationTypes;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.interfaces.ItemClickListener;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.birdaha.R;
 
@@ -59,17 +66,61 @@ public class HomePageFragment extends Fragment {
 
         ImageSlider imageSlider = view.findViewById(R.id.image_slider);
         ArrayList<SlideModel> slideModels = new ArrayList<>();
-        slideModels.add(new SlideModel(R.drawable.icardi2, ScaleTypes.CENTER_CROP));
-        slideModels.add(new SlideModel(R.drawable.icardi, ScaleTypes.FIT));
-        slideModels.add(new SlideModel(R.drawable.img, ScaleTypes.FIT));
-        slideModels.add(new SlideModel(R.drawable.img_1, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.icardi2, "Icardi", ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel(R.drawable.icardi, "Icardi baba", ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.img,"Etkinlik 1", ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.img_1,"Etkinlik 2", ScaleTypes.FIT));
 
 
         imageSlider.setImageList(slideModels, ScaleTypes.FIT);
+        imageSlider.setSlideAnimation(AnimationTypes.ZOOM_OUT);
+
+        imageSlider.setItemClickListener(new ItemClickListener() {
+    @Override
+        public void onItemSelected(int i) {
+
+        // Get the Vibrator service
+        Vibrator vibrator = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
+        if (vibrator != null && vibrator.hasVibrator()) {
+            // Create a vibration effect
+            // You can customize this effect as per your preference
+            VibrationEffect effect = VibrationEffect.createOneShot(15, VibrationEffect.DEFAULT_AMPLITUDE);
+            vibrator.vibrate(effect);
+        }
+            SlideModel currentEvent = slideModels.get(i);
+            AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+            LayoutInflater inflater = LayoutInflater.from(view.getContext());
+            View overlayView = inflater.inflate(R.layout.event_detail_overlay, null);
+            ImageView imageView = overlayView.findViewById(R.id.event_detail_image);
+            TextView detail = overlayView.findViewById(R.id.event_detail);
+            imageView.setImageResource(currentEvent.getImagePath());
+            detail.setText(currentEvent.getTitle());
+
+            builder.setView(overlayView);
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+    }
+
+    @Override
+    public void doubleClick(int position) {
+        // Implement your logic for double click here
+    }
+});
 
         // Fragment transaction should be here
         getChildFragmentManager().beginTransaction()
                 .replace(R.id.announcement_container, new GeneralAnnouncementFragment())
                 .commit();
     }
+
+
+    private void showImageOverlay(String imageUrlOrResource) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.events_view, null);
+        ImageView imageView = dialogView.findViewById(R.id.event_imageView);
+
+    }
+
 }
