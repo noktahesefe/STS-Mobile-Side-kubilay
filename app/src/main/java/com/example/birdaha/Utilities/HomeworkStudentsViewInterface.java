aa479c64dc06c7a1b24b59d3922d2d382d47669c
meteahmetyakar <meteahmetyakar@gmail.com>
2023-12-29 23:26:27 +0300

    merge

--- app/src/main/java/com/example/birdaha/Utilities/HomeworkStudentsViewInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.birdaha.Utilities;
-
-import android.view.View;
-
-import com.example.birdaha.General.StudentModel;
-
-public interface HomeworkStudentsViewInterface {
-    void onHomeworkStudentItemClick(StudentModel clickedItem, View view) throws InterruptedException;
-}
